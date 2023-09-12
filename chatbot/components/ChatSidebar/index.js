d311ae478ced453d4a4104c6a939768db91e1d1d
Sai-Krishna1001 <geddasaikrishna@gmail.com>
2023-09-13 01:02:30 +0530

    login-ui-updated

---
+++ chatbot/components/ChatSidebar/index.js
@@ -0,0 +1,1 @@
+import { ChatSidebar } from "./ChatSidebar";
--- chatbot/pages/chat/[[...chatId]].js
+++ chatbot/pages/chat/[[...chatId]].js
@@ -1,11 +1,41 @@
+import { ChatSidebar } from "components/ChatSidebar/ChatSidebar";
 import Head from "next/head";
-export default function Home() {
+import { useState } from "react";
+export default function ChatPage() {
+    const [messageText, setMessageText] = useState("");
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      console.log("MESSAGE: ", messageText);
+    }
     return (
-      <div>
+      <>
         <Head>
-          <title>Next JS ChatGPT Starter</title>
+          <title>New Chat</title>
         </Head>
-        <h1>Welcome to the chat id page</h1>
-      </div>
+        <div className="grid h-screen grid-cols-[260px_1fr]">
+          <ChatSidebar />
+          <div className="bg-gray-700 flex flex-col">
+            <div className="flex-1">Chat window</div>
+            <footer className="bg-gray-800 p-10">
+              <form onSubmit={handleSubmit}>
+                <fieldset className="flex gap-2">
+                  <textarea 
+                    value={messageText}
+                    onChange={(e) => setMessageText(e.target.value)}
+                    placeholder="Send a message..."
+                    className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"></textarea>
+                  <button 
+                    type="submit"
+                    className="btn"
+                  >
+                    Send
+                  </button>
+                </fieldset>
+              </form>
+            </footer>
+          </div>
+        </div>
+      </>
     );
   }

7e6c40a9440d48c76165320dc169c3412ba08aa4
Sai-Krishna1001 <geddasaikrishna@gmail.com>
2023-09-13 12:42:47 +0530

    'setupOpenAIKey

--- chatbot/pages/chat/[[...chatId]].js
+++ chatbot/pages/chat/[[...chatId]].js
@@ -2,12 +2,29 @@ import { ChatSidebar } from "components/ChatSidebar/ChatSidebar";
 import Head from "next/head";
 import { useState } from "react";
 export default function ChatPage() {
+    const [incomingMessage, setIncomingMessage] = useState("");
     const [messageText, setMessageText] = useState("");
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
       e.preventDefault();
+      console.log("MESSAGE TEXT: ", messageText);
+      const response  = await fetch(`/api/chat/sendMessage`,{
+        method: "POST",
+        headers:{
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({message: messageText}),
+      });
+      const data = response.body;
+      if(!data){
+        return "No data";
+      }
+      const reader = data.getReader();
+      await streamReader(reader, (message) => {
+        console.log("MESSAGE: ", message);
+        setIncomingMessage((s) => `${s}${message.content}`)
+      });
+    };
 
-      console.log("MESSAGE: ", messageText);
-    }
     return (
       <>
         <Head>
@@ -16,7 +33,7 @@ export default function ChatPage() {
         <div className="grid h-screen grid-cols-[260px_1fr]">
           <ChatSidebar />
           <div className="bg-gray-700 flex flex-col">
-            <div className="flex-1">Chat window</div>
+            <div className="flex-1 text-white">{incomingMessage}</div>
             <footer className="bg-gray-800 p-10">
               <form onSubmit={handleSubmit}>
                 <fieldset className="flex gap-2">
